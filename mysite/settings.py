527a11d3d0cf061fdf379a37e896ba86a77a06b0
Boyd Ching <chingboyd@gmail.com>
2017-12-17 17:52:01 +0900

    add header link placement

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/blog/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, "static"),
+#    os.path.join(BASE_DIR, "blog/static"),
+#]
