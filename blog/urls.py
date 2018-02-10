2186697b350ad3be77d6c467225438090607f031
Boyd Ching <chingboyd@gmail.com>
2018-02-10 15:44:57 +0900

    refactor layout
    * add footer
    * blog is split in left and right columns
    * post list is consistent on blog post transition
    * header rework

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^$', views.post_recent, name='post_recent'),
+    url(r'^$', views.base, name='base'),
+    #url(r'^$', views.post_list, name='post_list'),
+    #url(r'^$', views.post_recent, name='post_recent'),
+    # url(r'^post/(\d+)/$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,51 @@
 # -*- coding: utf-8 -*-
 #from __future__ import unicode_literals
+import json
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.http import HttpResponse
 from .models import Post
 from .forms import PostForm
 
-def post_list(recent):
+from snippets.template.ajaxsnippet import render_block_to_string
+from django.template import Context
+
+from datetime import date, datetime
+
+def json_serial(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError('Type %s not serializable' % type(obj))
+
+def get_post_list(self):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return posts
+
+def base(request):
+    posts = get_post_list(None);
+    return render(request, 'blog/base.html', {'posts': posts})
+
+def post_list(request):
+    posts = getPostList()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_recent(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).latest('published_date')
-    return render(request, 'blog/post_list.html', 
-        {'post': posts})
+    return render(request, 'blog/post_recent.html', {'post': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.method == "GET":
+        post = get_object_or_404(Post, pk=pk)
+        post_list = get_post_list(None)
+
+        return render(request, 'blog/post_detail.html', {'post': post, 'post_list': post_list})
+
+    else:
+        return HttpResponse(
+            json.dumps({'post error': 'A problem occurred looking for the post'}),
+            content_type = 'application/json'
+        )
 
 def post_new(request):
     if request.method == "POST":
